4fc034c04381ddc8d8e984c8ff8b5238f00bdf98
Minhpng <92564285+Lightrose142@users.noreply.github.com>
2022-11-16 15:20:27 +0700

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
-	return <div className="App"></div>
+	return (
+		<Routes>
+			<Route path="/" element={<h1>Home</h1>} />
+			<Route path="/new" element={<h1>New</h1>} />
+			<Route path="/:id">
+				<Route index element={<h1>Show</h1>} />
+				<Route path="edit" element={<h1>Edit</h1>} />
+			</Route>
+
+			<Route path="*" element={<Navigate to="/" />} />
+		</Routes>
+	)
 }
 
 export default App
