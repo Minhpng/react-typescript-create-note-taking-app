8d4fc937729d63025dfaed656982ac2ec2a2439e
Minhpng <92564285+Lightrose142@users.noreply.github.com>
2022-11-17 20:04:29 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,59 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useMemo } from "react"
+import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 import NewNote from "./NewNote"
+import useLocalStorage from "./useLocalStorage"
+
+export type RawNote = {
+	id: string
+} & RawNoteData
+
+export type RawNoteData = {
+	title: string
+	markdown: string
+	tagIds: string[]
+}
+export type Note = {
+	id: string
+} & NoteData
+
+export type NoteData = {
+	title: string
+	markdown: string
+	tags: Tag[]
+}
+
+export type Tag = {
+	id: string
+	label: string
+}
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<h1>Home</h1>} />
-			<Route path="/new" element={<NewNote />} />
-			<Route path="/:id">
-				<Route index element={<h1>Show</h1>} />
-				<Route path="edit" element={<h1>Edit</h1>} />
-			</Route>
+	const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+	const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
 
-			<Route path="*" element={<Navigate to="/" />} />
-		</Routes>
+	const notesWithTags = useMemo(() => {
+		return notes.map((note) => {
+			return { ...note, tags: tags.filter((tag) => note.tagIds.includes(tag.id)) }
+		})
+	}, [notes, tags])
+
+	console.log(notesWithTags)
+
+	return (
+		<Container className="my-4">
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>} />
+				<Route path="/new" element={<NewNote />} />
+				<Route path="/:id">
+					<Route index element={<h1>Show</h1>} />
+					<Route path="edit" element={<h1>Edit</h1>} />
+				</Route>
+
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</Container>
 	)
 }
 
